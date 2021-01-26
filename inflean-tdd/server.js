eee71e515af79270023c565783b01e78b953f6bd
awjae <djwotmd@gmail.com>
2021-01-26 23:13:19 +0900

    set express router and controller

--- inflean-tdd/server.js
+++ inflean-tdd/server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 
-const PORT = 8080;
+const PORT = 5000;
 const HOST = '0.0.0.0';
 
 //App
 const app = express();
+const prodictRoutes = require('./routes');
+
+//middleware
+app.use(express.json());
+app.use('/api/products', prodictRoutes);
+
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
