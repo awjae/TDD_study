4acdf7bd7bee641d480e142f49523f113391015d
awjae <djwotmd@gmail.com>
2022-02-28 00:55:44 +0900

    mmkdir

--- react-shop-test-app/src/App.test.js
+++ react-shop-test-app/src/App.test.js
@@ -5,4 +5,10 @@ test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+
+  // const lintTest = screen.getByRole("button", {
+  //   name: 'lineTest'
+  // })
+
+  // expect(lintTest).toHaveTextContent('lintTest')
 });
